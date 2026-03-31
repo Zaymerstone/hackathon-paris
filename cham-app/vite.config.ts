1dbf45afc7ece5ce6179dad7842dff01fb5551a8
Egor Nesterov <egor.nesterov2002@gmail.com>
2026-03-31 18:14:50 +0200

    fix

--- cham-app/vite.config.ts
+++ cham-app/vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: env.VITE_BASE_PATH || '/',
-    plugins: [react(), tailwindcss()],
-  }
-})
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || "/hackathon-paris",
+  };
+});
